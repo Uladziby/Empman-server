ccda9c49439fa5ca7154829540ff14d8b7e6b903
Vlad Yanushevsky <yanushevskyv@gmail.com>
2022-01-31 03:06:33 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import router from "./router";
+import { DotenvConfigOptions } from "dotenv";
 
 const port = process.env.PORT || 4040;
 
 const app = express();
-app.use(cors())
+app.use(cors({
+  origin: 'https://empman-uladziby.herokuapp.com'
+}))
 app.use(express.json());
 app.use("", router);
 
