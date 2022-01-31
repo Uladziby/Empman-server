398b8820b8f9bbbac6f147dfca9c34fd1bad12b6
Vlad Yanushevsky <yanushevskyv@gmail.com>
2022-01-31 03:53:53 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import cors from "cors";
 import router from "./router";
+import proxy from "http-proxy-middleware"
 import { DotenvConfigOptions } from "dotenv";
 
-const port = process.env.PORT || 4040;
+const port = process.env.PORT || 8080;
 
 const app = express();
+
 app.use(cors({
-  origin: 'https://empman-uladziby.herokuapp.com'
+  origin: 'https://empman-uladziby.herokuapp.com',
+  credentials: true,
 }))
 app.use(express.json());
 app.use("", router);
